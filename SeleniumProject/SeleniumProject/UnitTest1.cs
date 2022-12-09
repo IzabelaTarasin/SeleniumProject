5b108e0ede1b898094cf17731ccdab1c7b61b230
DESKTOP-D7QM33C\Izabela <izabela.tarasin@gmail.com>
2022-12-09 16:24:02 +0100

    added selenium and chromedriver

--- SeleniumProject/SeleniumProject/UnitTest1.cs
+++ SeleniumProject/SeleniumProject/UnitTest1.cs
@@ -1,16 +1,31 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 namespace SeleniumProject
 {
+   
     public class Tests
     {
+        IWebDriver driver;
+
         [SetUp]
         public void Setup()
         {
+            driver = new ChromeDriver("C:\\Users\\Izabela\\Projects\\Selenium\\SeleniumProject\\SeleniumProject\\Resources");
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            driver.Navigate().GoToUrl("https://google.pl");
+        }
+
+        [TearDown]
+        public void QuitDriver()
+        {
+            driver.Quit(); //close web browser + driver
+            //driver.Close(); //close web browser
         }
     }
 }
